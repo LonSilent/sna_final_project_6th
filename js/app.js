3ded1461e7629e487c075ca73fbde7ad4c0bcd51
Ray0427 <ray13242000@gmail.com>
2015-01-03 21:45:37 +0800

    remove login

--- js/app.js
+++ js/app.js
@@ -87,7 +87,7 @@ function statusChangeCallback(response) {
   	FB.api('/me', function(response) {
   		console.log('Successful login for: ' + response.name);
   		document.getElementById('status').innerHTML =
-  		'Thanks for logging in, ' + response.name + '!';
+  		'Welcome, ' + response.name + '!';
   	});
   }
 
