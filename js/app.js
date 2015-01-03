0952e6c2ee074978cca5fbb6dbf0f0b18712c1a4
Ray0427 <ray13242000@gmail.com>
2015-01-03 22:01:17 +0800

    ID

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ function statusChangeCallback(response) {
 
   window.fbAsyncInit = function() {
   	FB.init({
-  		appId      : '801180729947075',
+  		appId      : '1038073482885028',
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
     xfbml      : true,  // parse social plugins on this page
