c5d6008fedde16abf6cf157809bd35b08eae6bb8
Ray0427 <Ray0427@users.noreply.github.com>
2015-06-16 21:13:18 +0800

    Update parse.js

--- js/parse.js
+++ js/parse.js
@@ -27,7 +27,7 @@ var Food_CommentObject= new food_CommentObject();
 Food_CommentObject.save({
   title:restaurantTitle,
   restaurant: restaurantName,
-  date:restaurantDate,
+  //date:restaurantDate,
   city:restaurantCity,
   county:restaurantCounty,
   address:restaurantAddress,
